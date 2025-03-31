0e7f0002811073f36c22645d7d276f317d2ec6b9
ITcarrot <luoyb9@mail2.sysu.edu.cn>
2025-03-31 15:00:16 +0800

    use std null context instead

--- pipeMT/profile.py
+++ pipeMT/profile.py
@@ -1,4 +1,5 @@
 import os
+import contextlib
 from typing import *
 
 PROFILER_TYPE = None
@@ -7,14 +8,8 @@ if os.environ.get('NSYS_PROFILING_SESSION_ID'):
     PROFILER_TYPE = 'nsys'
     import nvtx
 
-class DummyContext:
-    def __enter__(self):
-        pass
-    def __exit__(self, *args):
-        pass
-
 def annotate(name: str, color: Optional[str] = None):
     if PROFILER_TYPE == 'nsys':
         return nvtx.annotate(name, color = color)
     else:
-        return DummyContext()
+        return contextlib.nullcontext()
